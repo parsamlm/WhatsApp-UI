3b2fc06d6248ac466f4afea17918b01e2836490e
Parsa Moslem <p.moslem1378@gmail.com>
2022-02-15 12:16:43 +0330

    status screen bug fixed

--- lib/pages/status.dart
+++ lib/pages/status.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Status extends StatefulWidget {
   const Status({Key? key}) : super(key: key);
@@ -14,89 +13,84 @@ class _StatusState extends State<Status> {
     return Container(
       color: const Color(0XFF121B22),
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Expanded(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 25.0, right: 25.0),
-                child: CircleAvatar(
-                  backgroundImage: const AssetImage("assets/person.png"),
-                  radius: 24.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    //todo status function
+                  },
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Container(
-                        margin: const EdgeInsets.only(left: 30.0, top: 22.0),
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle,
+                      CircleAvatar(
+                        backgroundImage: const AssetImage("assets/profile.jpg"),
+                        radius: 24.0,
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 30.0, top: 30.0),
+                          child: const Icon(
+                            Icons.add_circle_rounded,
+                            color: Color(0XFF00ab85),
+                          ),
                         ),
-                        child: const Icon(
-                          Icons.add_circle_rounded,
-                          color: Color(0XFF00ab85),
-                          size: 24,
+                      ),
+                      const SizedBox(width: 15.0),
+                      Container(
+                        margin: const EdgeInsets.only(top: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "My status",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 5.0),
+                            Text(
+                              "Tap to add status update",
+                              style: TextStyle(
+                                color: Color(0XFF87929B),
+                                fontSize: 13.0,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                  backgroundColor: const Color(0XFF637885),
                 ),
               ),
-              Padding(
-                  padding: const EdgeInsets.only(top: 5.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const <Widget>[
-                      Text(
-                        "My status",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 5.0),
-                      Text(
-                        "Tap to add status update",
-                        style: TextStyle(
-                          color: Color(0XFF87929B),
-                          fontSize: 13.0,
-                        ),
-                      ),
-                    ],
-                  )),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0.0, 0.0, 5.0, 0.0),
-                      child: FloatingActionButton.small(
-                        onPressed: () {
-                          //todo 'status' floatingActionButton function
-                        },
-                        backgroundColor: const Color(0XFF233239),
-                        child: const Icon(Icons.create_rounded),
-                      ),
-                    ),
-                    const SizedBox(height: 10.0),
-                    FloatingActionButton(
-                        onPressed: () {
-                          //todo 'camera' floatingActionButton function
-                        },
-                        backgroundColor: const Color(0XFF02a786),
-                        child: const Icon(Icons.photo_camera_rounded)),
-                  ],
-                ),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton.small(
+                    onPressed: (){
+
+                  },
+                    backgroundColor: const Color(0XFF233239),
+                    child: const Icon(Icons.create_rounded)
+                  ),
+                  const SizedBox(height: 10.0),
+                  FloatingActionButton(
+                      onPressed: (){
+
+                      },
+                      backgroundColor: const Color(0XFF02a786),
+                      child: const Icon(Icons.photo_camera_rounded)
+                  )
+                ],
               )
-            ],
-          ),
-        ),
+
+
+            ]),
       ),
     );
   }
